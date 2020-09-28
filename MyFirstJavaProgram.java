c1756b4ed2d3433c1f3ee3cf9b292c7da8012e75
lepidu <55885941+lepidu@users.noreply.github.com>
2020-09-28 09:59:16 -0500

    Add files via upload

---
+++ MyFirstJavaProgram.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package myfirstjavaprogram;
+
+/*This is my first java program
+ *This will print 'Hellos World' as the output
+ * @author Leisly Pino
+ */
+public class MyFirstJavaProgram {
+/*This is my first java program
+ *This will print 'Hellos World' as the output
+ * @author Leisly Pino
+ */
+    public static void main(String[] args) {
+        System.out.println("Hello World"); // prints Hello World
+    } //end of main
+    
+} //end of MyFirstProgram Class
